9de83f274e0bc8f5e53da18a163dd7352271ec9e
jerryfang313 <jerryfang313@gmail.com>
2018-05-31 16:07:40 -0700

    add comment on line 13.

--- MATLAB/gcode_vary_power_multi.m
+++ MATLAB/gcode_vary_power_multi.m
@@ -10,7 +10,7 @@ function gcode_vary_power()
 %        height - focal plane (Z)
 
 % Output: Gcode - X Y Z F S
-
+%test test
 %% adjust parameters here
 char = 'C'; % cut method. D: Discrete. C: continuous.
 
@@ -33,7 +33,7 @@ s_min = 100; % min laser power
 s_max = 1000; % max laser power
 fastF = 5000; % holder for G0
 
-gap = 1; % gap distance between each cut 
+gap = 1; % gap distance between each cut
 
 %%
 row_last = mod(num,row);
@@ -47,7 +47,7 @@ matf = [];
 
 % get the starting position for each block
 if row_last == 0
-    
+
     for i = 1:(num/row)
         for j = 1:row
             %[X Y F S]
@@ -58,7 +58,7 @@ if row_last == 0
             mat = [mat;mat_temp];
         end
     end
-    
+
 else
     for i = 1:col-1
         for j = 1:row
@@ -68,7 +68,7 @@ else
                          0];
             mat = [mat;mat_temp];
         end
-        
+
         for q = 1:row_last
             mat_temp11 = [pos(1)+(gap_hori*(col-1)) ...
                           pos(2)+(gap_vert*(q-1)) ...
@@ -94,31 +94,31 @@ for ii = 1:size(mat,1)
         mat_temp2_C = [x_ini+(len_step*jj)...
                      y_ini ...
                      speed ...
-                     s_min+(s_block*(ii-1))+(s_step*(jj-1))]; 
-                 
-        mat_temp2f_C = [mat_temp2f_C;mat_temp2_C];    
-        
+                     s_min+(s_block*(ii-1))+(s_step*(jj-1))];
+
+        mat_temp2f_C = [mat_temp2f_C;mat_temp2_C];
+
         %discrete cut
         mat_temp2_D1 = [x_ini+(len_step*jj)-gap...
                        y_ini ...
                        speed ...
-                       s_min+(s_block*(ii-1))+(s_step*(jj-1))]; 
-                   
+                       s_min+(s_block*(ii-1))+(s_step*(jj-1))];
+
         mat_temp2_D2 = [x_ini+(len_step*jj)...
                         y_ini ...
                         speed ...
-                        0]; 
+                        0];
         mat_temp2_D = [mat_temp2_D1;mat_temp2_D2];
         mat_temp2f_D = [mat_temp2f_D;mat_temp2_D];
-         
+
     end
-    
+
     matw_C = [mat(ii,:);mat_temp2f_C];
     matC = [matC;matw_C];
-    
+
     matw_D = [mat(ii,:);mat_temp2f_D];
     matD = [matD;matw_D];
-    
+
 end
 
 if strcmp(char,'C') || strcmp(char,'c')
@@ -158,4 +158,3 @@ fclose(fileID);
 
 
 end
-
